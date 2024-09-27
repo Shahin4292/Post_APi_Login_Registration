1c76e93de18d2fdd70e0791f3d9a6cee2da8008d
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-09-27 12:14:34 +0600

    post registration screen done

---
+++ lib/Screen/post_reg_screen.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  registerUser(String email, String password) async {
+    Uri url = Uri.parse("https://jsonplaceholder.typicode.com/users");
+    var data = {
+      "email": email,
+      "password": password,
+    };
+    try {
+      var response = await http.post(url, body: data);
+      if (response.statusCode == 201) {
+        var jsonData = jsonDecode(response.body);
+        print(jsonData);
+        print("Register Successful");
+      } else {
+        var error = jsonDecode(response.body);
+        print("Unable to register: ${error['error']}");
+      }
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Log Reg Post Api"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: "Email",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                // enabledBorder: OutlineInputBorder(
+                //   borderRadius: BorderRadius.circular(20),
+                // ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                hintText: "Password",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                // enabledBorder: OutlineInputBorder(
+                //   borderRadius: BorderRadius.circular(20),
+                // ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            GestureDetector(
+              onTap: () {
+                registerUser(emailController.text.toString(),
+                    passwordController.text.toString());
+              },
+              child: Container(
+                alignment: Alignment.center,
+                width: MediaQuery.sizeOf(context).width,
+                height: 55,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.blue),
+                child: const Text(
+                  "Register",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
